e89e6c6c9f95dfd5fd9be531aeffbff7dd246e8d
Achyut197 <126572604+Achyut197@users.noreply.github.com>
2023-08-02 10:29:01 +0530

    Add files via upload

---
+++ Challenges/Day_1/firstscriptday1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# here in this script we learned about 'echo` to print a message of your choice,declares variables and assigns values to them,two variables (numbers) as input and prints their sum using those variables
+# least three different built-in variables to display relevant information.
+# wildcards to list all the files with a specific extension in a directory.
